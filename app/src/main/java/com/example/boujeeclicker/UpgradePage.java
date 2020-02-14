718a4473749214447d0c869860a5462e0b857f16
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2020-02-13 21:29:36 -0500

    Added Second Screen for Upgrades Part 2

---
+++ app/src/main/java/com/example/boujeeclicker/UpgradePage.java
@@ -0,0 +1,14 @@
+package com.example.boujeeclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpgradePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrade_page);
+    }
+}
