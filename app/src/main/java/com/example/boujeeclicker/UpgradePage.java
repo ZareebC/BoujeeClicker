e2800a842fdfe0e9ea5045df699fb7deb5425e58
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2020-02-17 22:16:15 -0500

    Added Back Button to Game

--- app/src/main/java/com/example/boujeeclicker/UpgradePage.java
+++ app/src/main/java/com/example/boujeeclicker/UpgradePage.java
@@ -2,13 +2,24 @@ package com.example.boujeeclicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpgradePage extends AppCompatActivity {
-
+    Button backClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade_page);
+        backClick = findViewById(R.id.backClick);
+        backClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UpgradePage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
