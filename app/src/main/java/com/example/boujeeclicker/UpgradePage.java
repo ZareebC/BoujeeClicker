3a4e066f3851c67e7a760de5600490833e13f133
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2020-02-18 20:21:36 -0500

    Intent Test

--- app/src/main/java/com/example/boujeeclicker/UpgradePage.java
+++ app/src/main/java/com/example/boujeeclicker/UpgradePage.java
@@ -6,14 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UpgradePage extends AppCompatActivity {
     Button backClick;
+    ImageView passive;
+    TextView textView;
+    String clickScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade_page);
         backClick = findViewById(R.id.backClick);
+        passive = findViewById(R.id.passive);
+        textView = findViewById(R.id.test);
+        passive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle extras = getIntent().getExtras();
+                if(extras!= null){
+                    clickScore = extras.getString("clickScore");
+                    textView.setText((""+clickScore));
+                }
+            }
+        });
         backClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
